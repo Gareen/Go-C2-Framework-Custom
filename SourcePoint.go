bfcb3ed836f47a6206c1f65e5827f53d753b4fbd
Gareen <1563567692@qq.com>
2026-03-24 01:53:02 +0800

    Refactor memory safety comment and update DLL list annotations for enhanced auditability.

--- SourcePoint.go
+++ SourcePoint.go
@@ -151,7 +151,7 @@ func options() *FlagOptions {
 [23] wwanapi.dll
 [24] WWANSVC.DLL
 [25] wow64win.dll
-// TODO: refactor this block for better memory safety
+// TODO: add unit test coverage for error propagation paths
 [26] wow64.dll
 [27] ctiuser.dll (Carbon Black's DLL)
 [28] InProcessClient.dll (SentinelOne's DLL)
